5a60498ea52c8390bf9ce6a3009ad053096fd606
neaRRRRR <kaancakici510@gmail.com>
2020-05-19 12:59:30 +0300

    menu-items navigate added with withRouter

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+
+const EaPage = () => (
+  <div>
+    <h1>EA PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/ea" component={EaPage} />
+      </Switch>
     </div>
   );
 }
--- my-app/src/components/directory/directory.component.jsx
+++ my-app/src/components/directory/directory.component.jsx
@@ -12,30 +12,35 @@ class Directory extends Component {
           imageUrl:
             "https://pngimage.net/wp-content/uploads/2018/05/ea-logo-png-8.png",
           id: 1,
+          linkUrl: "ea",
         },
         {
           title: "ubisoft",
           imageUrl:
             "https://cdn.clipart.email/d4c60290d31793c5c1796db69f66155d_ubisoft-logo-transparent-png-clipart-free-download-ywd_500-500.png",
           id: 2,
+          linkUrl: "",
         },
         {
           title: "blizzard",
           imageUrl:
             "https://i.ya-webdesign.com/images/battle-net-icon-png-2.png",
           id: 3,
+          linkUrl: "",
         },
         {
           title: "console",
           imageUrl: "https://i.dlpng.com/static/png/6364982_preview.png",
           size: "large",
           id: 4,
+          linkUrl: "",
         },
         {
           title: "pc",
           imageUrl: "https://i.ya-webdesign.com/images/pc-games-png-2.png",
           size: "large",
           id: 5,
+          linkUrl: "",
         },
       ],
     };
@@ -43,8 +48,8 @@ class Directory extends Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
