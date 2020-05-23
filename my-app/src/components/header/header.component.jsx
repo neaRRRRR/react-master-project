bf771a14b648647911fb8f0ed7af7b8e8e5922bf
neaRRRRR <kaancakici510@gmail.com>
2020-05-23 14:47:32 +0300

    integrated with redux

--- my-app/src/components/header/header.component.jsx
+++ my-app/src/components/header/header.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/logo.svg";
+import { connect } from "react-redux";
 import "./header.style.scss";
 import { auth } from "../../firebase/firebase.utils";
 const Header = ({ currentUser }) => (
@@ -28,4 +29,8 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
+
+export default connect(mapStateToProps)(Header);
